f56c46041fa1ef3d0b494ccb653fe69bb32b4672
Noureddine Tanjaoui <noureddine.nt3@gmail.com>
2024-05-27 19:12:45 +0100

    added upload functionalty with uploadthing (maybe swith to s3 later)

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -5,19 +5,42 @@ import { Image, Loader2, MousePointerSquareDashed } from 'lucide-react'
 import React, { useState, useTransition } from 'react'
 import Dropzone, { FileRejection } from 'react-dropzone'
 import { Progress } from "@/components/ui/progress"
+import { useUploadThing } from '@/lib/uploadthing'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/ui/use-toast'
 
 const Page = () => {
 
+  const { toast } = useToast()
   const [isDragOver, SetIsDragOver] = useState<boolean>(false)
-  const onDropRejected = () => {
-    
-  }
-  const onDropAccepted = () => {
-
-  }
-  const isUploading = false
   const [isPending, startTransition] = useTransition()
   const [uploadProgress, setUploadProgress] = useState(0)
+  const router = useRouter()
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId
+      startTransition(() => {
+        router.push(`/configure/design?id=${configId}`)
+      })
+    },
+    onUploadProgress(p) {
+      setUploadProgress(p)
+    }
+  })
+  const onDropRejected = (rejectedFile: FileRejection[]) => {
+    const [file] = rejectedFile
+    SetIsDragOver(false)
+    toast({
+      title: `${file.file.type} type is not supported !`,
+      description: "Please choose a PNG, JPG or JPEG image type !",
+      variant: "destructive"
+    })
+  }
+  const onDropAccepted = (acceptedFile: File[]) => {
+    startUpload(acceptedFile, { configId: undefined })
+    SetIsDragOver(false)
+  }
+
 
   return (
     <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center", {
