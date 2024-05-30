edbc9100ba13c1c96f39690d9ecf2287769ea838
Noureddine Tanjaoui <noureddine.nt3@gmail.com>
2024-05-30 16:57:46 +0100

    working on the next step after uploading imagne, added sharp to get image metadata and trying to configure it

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,10 +1,32 @@
+import { db } from '@/db'
+import { notFound } from 'next/navigation'
 import React from 'react'
+import DesignConfigurator from './DesignConfigurator'
 
-const Page = () => {
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined,
+  }
+}
+
+const Page = async ({searchParams}: PageProps ) => {
+
+  const {id} = searchParams
+  if(!id || typeof id !== 'string'){
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: {id},
+  })
+  if(!configuration) {
+    return notFound();
+  }
+
+  const {imageUrl, width, height} = configuration
+
   return (
-    <div>
-      
-    </div>
+    <DesignConfigurator />
   )
 }
 
