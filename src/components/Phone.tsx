71d35a9193e5adbe782cd0bb2a43ddcd18949c39
Noureddine Tanjaoui <noureddine.nt3@gmail.com>
2024-06-27 15:09:26 +0100

    finished custom case preview component, will study and integrate  stripe next

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -11,7 +11,7 @@ const Phone = ({imgSrc, dark = false, className, ... props}: PhoneProps) => {
     <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {... props}>
       <img className='pointer-events-none z-50 select-none' src={dark ? "/phone-template-dark-edges.png" : "/phone-template-white-edges.png"} alt='phone image'/>
       <div className='absolute -z-10 inset-0'>
-        <img className='object-cover' src={imgSrc} alt="overlaying phone img" />
+        <img className='object-cover min-w-full min-h-full' src={imgSrc} alt="overlaying phone img" />
       </div>
     </div>
   )
